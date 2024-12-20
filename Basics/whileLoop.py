1b8df3b4e3548c0b222104b1b333bc4121d88a5c
AfiaMaham <afiamaham08@gmail.com>
2024-12-20 22:07:44 +0500

    whileLoop

---
+++ Basics/whileLoop.py
@@ -0,0 +1,9 @@
+
+# Print User Input Times:
+# Ask the user for a number, and print "Hello, World!" that many times using a while loop.
+
+num = int(input("Enter number: "))
+
+while num != 0:
+    print("Hello, World!")
+    num -= 1
