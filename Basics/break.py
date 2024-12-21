c371979966f2438c3394caa37e031e2e1d77b209
AfiaMaham <afiamaham08@gmail.com>
2024-12-21 17:05:16 +0500

    break statement

---
+++ Basics/break.py
@@ -0,0 +1,11 @@
+# Break Statement Assignments
+
+# Stop at Target: Write a program to print numbers from 1 to 50, 
+# but stop the loop when the number 25 is reached using the break statement.
+
+for i in range(1,51):
+    print(i)
+    if i == 25:
+        break
+    
+
