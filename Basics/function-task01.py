41957b7f63503f3b0a670847ae526564ee6dfa6a
AfiaMaham <afiamaham08@gmail.com>
2024-12-26 15:57:04 +0500

    function

---
+++ Basics/function-task01.py
@@ -0,0 +1,16 @@
+# Check Even or Odd
+# Write a function that takes an integer and returns whether it is even or odd.
+
+def evenOrOdd (num):
+    if(num%2==0):
+        return "Number is even"
+    else:
+        return "number is odd"
+    
+num = int(input("Enter number: "))
+print(evenOrOdd(num))
+
+
+    
+
+
