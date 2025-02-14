e4fc7650f66d6390656cf4d5b506e734500322a2
AfiaMaham <afiamaham08@gmail.com>
2025-02-14 13:42:08 +0500

    task04

---
+++ Turtle-library/task04.py
@@ -0,0 +1,14 @@
+import turtle as t
+
+s = t.Screen()
+s.bgcolor('black')
+t.fillcolor("red")
+t.begin_fill()
+t.left(50)
+t.fd(120)
+t.circle(45,200)
+t.lt(221)
+t.circle(45, 200)
+t.fd(130)
+t.end_fill()
+t.done()
